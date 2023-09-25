e832d4b4c695155b1527b5e97074992fe4e30d13
David Puerta Martín <puertadavid96182@gmail.com>
2023-09-24 20:12:17 -0400

    update pydantic settings

--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     redis_host: str
     redis_port: int
 
-    class Config:
+    model_config = SettingsConfigDict(
         env_file = ".env"
+    )
 
